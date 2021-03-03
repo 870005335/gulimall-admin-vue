facbf895352c498101a6f92dd9181eed91f3de6e
liubin <870005335@qq.com>
2021-03-03 19:24:14 +0800

    新增分类

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -1,34 +1,47 @@
 <template>
-  <el-tree
-    :data="menu"
-    :props="defaultProps"
-    :expand-on-click-node="false"
-    show-checkbox
-    node-key="catId"
-    :default-expanded-keys="expandedKey"
-  >
-    <span class="custom-tree-node" slot-scope="{ node, data }">
-      <span>{{ node.label }}</span>
-      <span>
-        <el-button
-          v-if="node.level <= 2"
-          type="text"
-          size="mini"
-          @click="() => append(data)"
-        >
-          修改
-        </el-button>
-        <el-button
-          v-if="node.childNodes.length == 0"
-          type="text"
-          size="mini"
-          @click="() => remove(node, data)"
-        >
-          删除
-        </el-button>
+  <div>
+    <el-tree
+      :data="menu"
+      :props="defaultProps"
+      :expand-on-click-node="false"
+      show-checkbox
+      node-key="catId"
+      :default-expanded-keys="expandedKey"
+    >
+      <span class="custom-tree-node" slot-scope="{ node, data }">
+        <span>{{ node.label }}</span>
+        <span>
+          <el-button
+            v-if="node.level <= 2"
+            type="text"
+            size="mini"
+            @click="() => append(data)"
+          >
+            新增
+          </el-button>
+          <el-button
+            v-if="node.childNodes.length == 0"
+            type="text"
+            size="mini"
+            @click="() => remove(node, data)"
+          >
+            删除
+          </el-button>
+        </span>
       </span>
-    </span>
-  </el-tree>
+    </el-tree>
+    <el-dialog title="提示" :visible.sync="dialogVisible" width="30%">
+      <el-form :model="category">
+        <el-form-item label="分类名称">
+          <el-input v-model="category.name" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addCategory">确 定</el-button>
+      </span>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
@@ -36,8 +49,10 @@ export default {
   components: {},
   data() {
     return {
+      category: {},
+      dialogVisible: false,
       menu: [],
-      expandedKey:[],
+      expandedKey: [],
       defaultProps: {
         children: "children",
         label: "name",
@@ -57,21 +72,25 @@ export default {
         }
       });
     },
-    append(data) {},
+    append(data) {
+      this.dialogVisible = true;
+      this.category.parentCid = data.catId;
+      this.category.catLevel = data.catLevel * 1 + 1;
+      this.category.showStatus = 1;
+      this.category.sort = 0;
+    },
+    addCategory() {
+      console.log(this.category);
+    },
     remove(node, data) {
       // 获取当前删除的节点id
-      console.log(data)
+      console.log(data);
       var ids = [data.catId];
-      var id = ids[0];
-      this.$confirm(
-        `是否删除[${data.name}]菜单?`,
-        "提示",
-        {
-          confirmButtonText: "确定",
-          cancelButtonText: "取消",
-          type: "warning",
-        }
-      )
+      this.$confirm(`是否删除[${data.name}]菜单?`, "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning",
+      })
         .then(() => {
           this.$http({
             url: this.$http.adornUrl("/product/category/delete"),
